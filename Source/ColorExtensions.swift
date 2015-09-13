afa162831242443d9d207e43d41fb244fad64152
Patrick Perini <pcperini@gmail.com>
2015-09-13 11:32:23 -0700

    .

---
+++ Source/ColorExtensions.swift
@@ -0,0 +1,60 @@
+//
+//  ColorExtensions.swift
+//  MiseEnPlace
+//
+//  Created by PATRICK PERINI on 9/12/15.
+//  Copyright © 2015 pcperini. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hexString: String) {
+        var rgbValue: UInt32 = 0
+        
+        let scanner = NSScanner(string: hexString)
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        
+        scanner.scanHexInt(&rgbValue)
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0xFF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0xFF) / 255.0,
+            alpha: 1.0)
+    }
+    
+    private func combine(rhs: UIColor, op: (CGFloat, CGFloat) -> CGFloat) -> UIColor {
+        var lhsRed: CGFloat = 0.0
+        var lhsGreen: CGFloat = 0.0
+        var lhsBlue: CGFloat = 0.0
+        var lhsAlpha: CGFloat = 0.0
+        
+        self.getRed(&lhsRed,
+            green: &lhsGreen,
+            blue: &lhsBlue,
+            alpha: &lhsAlpha)
+        
+        var rhsRed: CGFloat = 0.0
+        var rhsGreen: CGFloat = 0.0
+        var rhsBlue: CGFloat = 0.0
+        
+        rhs.getRed(&rhsRed,
+            green: &rhsGreen,
+            blue: &rhsBlue,
+            alpha: nil)
+        
+        return UIColor(red: op(lhsRed, rhsRed),
+            green: op(lhsGreen, rhsGreen),
+            blue: op(lhsBlue, rhsBlue),
+            alpha: lhsAlpha)
+    }
+}
+
+func +(lhs: UIColor, rhs: UIColor) -> UIColor {
+    return lhs.combine(rhs, op: +)
+}
+
+func -(lhs: UIColor, rhs: UIColor) -> UIColor {
+    return lhs.combine(rhs, op: -)
+}
--- Source/TableViewPagination.swift
+++ Source/TableViewPagination.swift
@@ -72,3 +72,66 @@ import UIScrollView_InfiniteScroll
         completion()
     }
 }
+
+@IBDesignable public extension UICollectionViewController {
+    // MARK: Properties
+    @IBInspectable var infiniteScrolls: Bool {
+        get { return objc_getAssociatedObject(self, "InfiniteScrolls") as? Bool ?? false }
+        set {
+            objc_setAssociatedObject(self,
+                "InfiniteScrolls",
+                newValue,
+                objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+            
+            if newValue {
+                self.collectionView?.addInfiniteScrollWithHandler { (_) in
+                    self.appendNextPage {
+                        self.collectionView?.finishInfiniteScroll()
+                    }
+                }
+            } else {
+                self.collectionView?.removeInfiniteScroll()
+            }
+        }
+    }
+    
+    @IBInspectable var refreshes: Bool {
+        get { return objc_getAssociatedObject(self, "Refreshes") as? Bool ?? false }
+        set {
+            objc_setAssociatedObject(self,
+                "Refreshes",
+                newValue,
+                objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+            
+            if newValue {
+                let refreshControl = UIRefreshControl()
+                refreshControl.addTarget(self,
+                    action: "refreshControlWasTriggered:",
+                    forControlEvents: .ValueChanged)
+                
+                self.collectionView?.addSubview(refreshControl)
+            } else {
+                let refreshControls = self.collectionView?.subviews.filter { $0.isKindOfClass(UIRefreshControl.self) }
+                refreshControls?.forEach { $0.removeFromSuperview() }
+            }
+        }
+    }
+    
+    // MARK: Responders
+    func refreshControlWasTriggered(sender: UIRefreshControl!) {
+        self.reloadData {
+            sender.endRefreshing()
+        }
+    }
+    
+    // MARK: Data Handlers
+    public func reloadData(completion: () -> Void = {}) {
+        self.collectionView?.reloadData()
+        completion()
+    }
+    
+    public func appendNextPage(completion: () -> Void = {}) {
+        self.collectionView?.reloadData()
+        completion()
+    }
+}
